25b8bc10d4f6c04ca0e1c7e63d227ce086db099b
KamilKaczmarekKMZ <kamil.kaczmarek.work@gmail.com>
2025-08-22 20:13:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// Import i inicjalizacja tła Hexagonal Grid
+import Grid1Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.16/build/backgrounds/grid1.cdn.min.js'
+
+const bg = Grid1Background(document.getElementById('webgl-canvas'))
+
+// Obsługa przycisku zmiany kolorów tła
+const button1 = document.getElementById('colors-btn')
+button1.addEventListener('click', () => {
+  bg.grid.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+  bg.grid.light1.color.set(0xffffff * Math.random())
+  bg.grid.light1.intensity = 500 + Math.random() * 1000
+  bg.grid.light2.color.set(0xffffff * Math.random())
+  bg.grid.light2.intensity = 250 + Math.random() * 250
+})
+
+// Oryginalny kod strony
 document.addEventListener('DOMContentLoaded', () => {
   const modelViewer = document.querySelector('model-viewer');
   const buttonContainer = document.querySelector('.button-container');
